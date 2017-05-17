79a1387a7e664d79bf4016e88044563c3ae4bf31
GMarks2000 <garemark@ed.amdsb.ca>
2017-05-17 14:22:07 -0400

    optios

--- BrickBreaker/Screens/OptionScreen.Designer.cs
+++ BrickBreaker/Screens/OptionScreen.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace SuperSnakeGame
+﻿namespace BrickBreaker
 {
-    partial class menuButton
+    partial class OptionScreen
     {
         /// <summary> 
         /// Required designer variable.
@@ -31,17 +31,19 @@
             this.easyButton = new System.Windows.Forms.Button();
             this.mediumButton = new System.Windows.Forms.Button();
             this.hardButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.menuButton = new System.Windows.Forms.Button();
             this.difficultyLabel = new System.Windows.Forms.Label();
             this.acceptButton = new System.Windows.Forms.Button();
             this.impossibleButton = new System.Windows.Forms.Button();
+            this.acceptLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // easyButton
             // 
             this.easyButton.BackColor = System.Drawing.SystemColors.Control;
             this.easyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.easyButton.Location = new System.Drawing.Point(113, 65);
+            this.easyButton.Location = new System.Drawing.Point(113, 135);
             this.easyButton.Name = "easyButton";
             this.easyButton.Size = new System.Drawing.Size(230, 67);
             this.easyButton.TabIndex = 1;
@@ -51,9 +53,9 @@
             // 
             // mediumButton
             // 
-            this.mediumButton.BackColor = System.Drawing.SystemColors.Control;
+            this.mediumButton.BackColor = System.Drawing.Color.Orange;
             this.mediumButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mediumButton.Location = new System.Drawing.Point(113, 159);
+            this.mediumButton.Location = new System.Drawing.Point(465, 135);
             this.mediumButton.Name = "mediumButton";
             this.mediumButton.Size = new System.Drawing.Size(230, 67);
             this.mediumButton.TabIndex = 2;
@@ -65,7 +67,7 @@
             // 
             this.hardButton.BackColor = System.Drawing.SystemColors.Control;
             this.hardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hardButton.Location = new System.Drawing.Point(113, 254);
+            this.hardButton.Location = new System.Drawing.Point(113, 242);
             this.hardButton.Name = "hardButton";
             this.hardButton.Size = new System.Drawing.Size(230, 67);
             this.hardButton.TabIndex = 3;
@@ -73,65 +75,94 @@
             this.hardButton.UseVisualStyleBackColor = false;
             this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
             // 
-            // button1
+            // menuButton
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(477, 460);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(230, 79);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Menu";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.menuButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuButton.ForeColor = System.Drawing.Color.Red;
+            this.menuButton.Location = new System.Drawing.Point(465, 454);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(230, 79);
+            this.menuButton.TabIndex = 6;
+            this.menuButton.Text = "Menu";
+            this.menuButton.UseVisualStyleBackColor = true;
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
             // 
             // difficultyLabel
             // 
             this.difficultyLabel.AutoSize = true;
             this.difficultyLabel.BackColor = System.Drawing.Color.Transparent;
-            this.difficultyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difficultyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.difficultyLabel.ForeColor = System.Drawing.SystemColors.Control;
-            this.difficultyLabel.Location = new System.Drawing.Point(443, 286);
+            this.difficultyLabel.Location = new System.Drawing.Point(442, 308);
             this.difficultyLabel.Name = "difficultyLabel";
-            this.difficultyLabel.Size = new System.Drawing.Size(0, 31);
+            this.difficultyLabel.Size = new System.Drawing.Size(0, 29);
             this.difficultyLabel.TabIndex = 5;
             // 
             // acceptButton
             // 
+            this.acceptButton.BackColor = System.Drawing.SystemColors.Control;
             this.acceptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.acceptButton.Location = new System.Drawing.Point(113, 460);
+            this.acceptButton.ForeColor = System.Drawing.Color.Lime;
+            this.acceptButton.Location = new System.Drawing.Point(113, 454);
             this.acceptButton.Name = "acceptButton";
-            this.acceptButton.Size = new System.Drawing.Size(230, 67);
-            this.acceptButton.TabIndex = 6;
+            this.acceptButton.Size = new System.Drawing.Size(230, 79);
+            this.acceptButton.TabIndex = 5;
             this.acceptButton.Text = "Accept";
-            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.UseVisualStyleBackColor = false;
             this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
             // 
             // impossibleButton
             // 
             this.impossibleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.impossibleButton.Location = new System.Drawing.Point(113, 358);
+            this.impossibleButton.Location = new System.Drawing.Point(465, 242);
             this.impossibleButton.Name = "impossibleButton";
             this.impossibleButton.Size = new System.Drawing.Size(230, 67);
-            this.impossibleButton.TabIndex = 7;
+            this.impossibleButton.TabIndex = 4;
             this.impossibleButton.Text = "Impossible";
             this.impossibleButton.UseVisualStyleBackColor = true;
             this.impossibleButton.Click += new System.EventHandler(this.impossibleButton_Click);
             // 
-            // menuButton
+            // acceptLabel
             // 
+            this.acceptLabel.AutoSize = true;
+            this.acceptLabel.BackColor = System.Drawing.Color.Transparent;
+            this.acceptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.acceptLabel.ForeColor = System.Drawing.Color.Black;
+            this.acceptLabel.Location = new System.Drawing.Point(107, 340);
+            this.acceptLabel.Name = "acceptLabel";
+            this.acceptLabel.Size = new System.Drawing.Size(0, 31);
+            this.acceptLabel.TabIndex = 8;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(220, 32);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(383, 42);
+            this.titleLabel.TabIndex = 9;
+            this.titleLabel.Text = "Select Your Difficulty";
+            // 
+            // OptionScreen
+            // 
+            this.AccessibleDescription = "";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::SuperSnakeGame.Properties.Resources.Brick_Breaker;
+            this.BackgroundImage = global::BrickBreaker.Properties.Resources.fadedBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.acceptLabel);
             this.Controls.Add(this.impossibleButton);
             this.Controls.Add(this.acceptButton);
             this.Controls.Add(this.difficultyLabel);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuButton);
             this.Controls.Add(this.hardButton);
             this.Controls.Add(this.mediumButton);
             this.Controls.Add(this.easyButton);
-            this.Name = "menuButton";
+            this.Name = "OptionScreen";
             this.Size = new System.Drawing.Size(785, 542);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.OptionScreen_PreviewKeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,9 +172,11 @@
         private System.Windows.Forms.Button easyButton;
         private System.Windows.Forms.Button mediumButton;
         private System.Windows.Forms.Button hardButton;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button menuButton;
         private System.Windows.Forms.Label difficultyLabel;
         private System.Windows.Forms.Button acceptButton;
         private System.Windows.Forms.Button impossibleButton;
+        private System.Windows.Forms.Label acceptLabel;
+        private System.Windows.Forms.Label titleLabel;
     }
 }
