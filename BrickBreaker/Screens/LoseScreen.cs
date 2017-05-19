1bf46e146cac7f05059d4df3010d95876d00dfcb
GMarks2000 <garemark@ed.amdsb.ca>
2017-05-19 14:07:35 -0400

    final

--- BrickBreaker/Screens/LoseScreen.cs
+++ BrickBreaker/Screens/LoseScreen.cs
@@ -193,7 +193,7 @@ namespace BrickBreaker
                         Form1.select.Stop();
                         Form1.select.Play();
 
-                        Highscore hs = new Highscore(nameText1.Text + nameText2.Text + nameText3.Text, Convert.ToString(Form1.currentLevel), Convert.ToString(Form1.currentScore));
+                        Highscore hs = new Highscore(nameText1.Text + nameText2.Text + nameText3.Text, Convert.ToString(GameScreen.currentLevel), Convert.ToString(Form1.currentScore));
 
                         hs.save(hs);
                         hs.saveScores(Form1.highscoreList);
